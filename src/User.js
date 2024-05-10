674c3bff7dc25dad150256051974a2abc2dfda20
Julesyasaby <166526374+Julesyasaby@users.noreply.github.com>
2024-05-10 16:01:46 +0100

    Modified Scooter Project
    Tried my best :)

--- src/User.js
+++ src/User.js
@@ -1,5 +1,21 @@
 class User {
   // User code here
+  constructor(username, password, age){
+    this.username = username
+    this.password = password
+    this.age = age
+    this.loggedIn = false
+  }
+  logged(password){
+if (this.password === password){
+  this.loggedIn = true;
+} else{
+  throw new Error("Incorrect password.")
+}
+  }
+  loggedout(){
+this.loggedIn = false
+  }
 }
 
 module.exports = User
